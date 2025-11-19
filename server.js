c79749d390a7535f9e2fd9b3426038c1690a142f
MATEUS MULLER BISCARO <mateus_biscaro@estudante.sesisenai.org.br>
2025-11-19 11:03:53 -0300

    API

---
+++ server.js
@@ -0,0 +1,79 @@
+import express, { response } from "express"
+import pg from 'pg'
+
+const { Pool } = pg
+
+const server = express();
+server.use(express.json())
+
+const sql = new Pool({
+    host: 'localhost',
+    user: 'local',
+    password: '12345',
+    database: 'chamados',
+    port: 5432
+
+})
+
+
+
+server.get('/users', async (req, res) => {
+    try {
+
+        const response = await sql.query('SELECT * FROM users')
+        return res.json({ results: response.rows, ok: true })
+
+    } catch (error) {
+        res.status(500).json({ message: error, ok: false })
+    }
+})
+
+
+server.post('/users', async (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+    const profile = req.body.profile;
+
+    try {
+        const response = await sql.query(
+            'INSERT INTO users (name, email, password, profile) VALUES ($1, $2, $3, $4)',
+            [name, email, password, profile]
+        )
+        res.status(201).json({ message: 'usuario criado com sucesso', ok: true })
+    } catch (error) {
+        res.status(500).json({ message: error, ok: false })
+    }
+
+    return res.send('usuario cadastrado com sucesso!')
+})
+
+server.post('/login', async (req, res) => {
+
+    const email = req.body.email;
+    const password = req.body.password;
+
+    try {
+        const response = await sql.query(
+            'SELECT * FROM users WHERE email = $1 AND password = $2',
+            [email, password]
+        )
+
+        if(response.rows.length === 0){
+            res.status(200).json({message:'usuario nao encontrado'})
+        }
+
+        
+
+        res.status(201).json({ message: 'Usuario logado com sucesso', ok: true })
+    } catch (error) {
+        res.status(500).json({ message: error, ok: false })
+    }
+
+    return res.send('usuario cadastrado com sucesso!')
+})
+
+
+server.listen(3000, () => {
+    console.log('tá rodando: http://localhost:3000/')
+})
